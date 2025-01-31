8b11852432e932fdffc4a638eee629fff8ae23ce
Pedro <borgespedro504@gmail.com>
2025-01-31 19:34:24 -0400

    Update ex1.py

--- list3_recursion/ex1.py
+++ list3_recursion/ex1.py
@@ -1,1 +1,10 @@
 print("||| Exercise 1 - Reverse string |||")
+
+def reverse_string(stri):
+    if len(stri) == 0:
+        return stri
+    return stri[-1] + reverse_string(stri[:-1]) 
+
+string = input("Type anything: ")
+print("Now we are going to show you what you writed but reversed:")
+print(reverse_string(string))
