a339404ca2fcd85c47140b7ee24e28424f289fed
Pedro <borgespedro504@gmail.com>
2025-02-03 08:54:02 -0400

    Add files via upload

---
+++ list3_recursion/ex2.py
@@ -0,0 +1,8 @@
+print("||| Exercise 2 - Sum numbers in a list |||")
+
+def sumlist(nlist):
+    return 0
+
+nest_list = [3,2,[11,30,3],1,[1,2],32,[1,2,[3,4,5]]]
+print("Now we are going to show you what you writed but reversed:")
+print(sumlist(nest_list))
