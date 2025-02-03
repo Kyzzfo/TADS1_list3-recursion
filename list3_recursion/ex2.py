16695877b8739d3e115ed39626ae74d0710004dd
Pedro <borgespedro504@gmail.com>
2025-02-03 13:54:29 -0400

    Update ex2.py

--- list3_recursion/ex2.py
+++ list3_recursion/ex2.py
@@ -4,11 +4,11 @@ def sumlist(nlist):
     sumn=0
     for number in nlist:
         if isinstance(number, list):
-                sumn += sumlist(number)
+            sumn += sumlist(number)
         else:
             sumn += number
     return sumn
-    
+
 nest_list = [3,2,[11,30,3],1,[1,2],32,[1,2,[3,4,5]]]
-print("Now we are going to show you what you writed but reversed:")
+print(f"The sum of all numbers in the list {nest_list} is:")
 print(sumlist(nest_list))
