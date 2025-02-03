416fb4eb8196c23a031e724f7c86602afef24b27
Pedro <borgespedro504@gmail.com>
2025-02-03 15:38:14 -0400

    Add files via upload

---
+++ list3_recursion/ex4.py
@@ -0,0 +1,10 @@
+print("||| Exercise 4 - Find the index of the highest element |||")
+
+list_n = [1,7,8,3,10,2,9]
+
+def high_number(n_list):
+    return 0
+
+print(f"Now we are going to show the index of the highest number in the list {list_n}:")
+print(high_number(list_n))
+print(len(list_n))
